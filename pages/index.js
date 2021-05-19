981968fa561b32034542cfc2ee1eebe96216e583
Akaljot Gujjar <akaljotgujjar@gmail.com>
2021-05-18 21:49:06 -0700

    added className

--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,22 @@ export default function Home() {
 
       <Navbar />
 
-      <div class="grid md:grid-cols-3 gap-4 m-10">
+      <div className="grid md:grid-cols-3 gap-4 m-10">
         {/* column one */}
-        <div class="col-span-2 bg-gradient-to-br from-blue-500 to-blue-800 rounded-lg max-h-20 p-5 shadow-2xl">
-          <h1 class="text-white text-center xl:text-3xl font-semibold">
+        <div className="col-span-2 bg-gradient-to-br from-blue-500 to-blue-800 rounded-lg max-h-20 p-5 shadow-2xl">
+          <h1 className="text-white text-center xl:text-3xl font-semibold">
             We Are GOBIND SARVAR, developing "Today's Learner... Tomorrow's
             Guide"
           </h1>
         </div>
         {/* column one */}
-        <div class="col-span-1 rounded-lg bg-gradient-to-br from-yellow-300 to-yellow-500 shadow-2xl">
-          <h1 class="text-center lg:text-3xl p-5 font-semibold">GOBIND SARVAR NEWS!</h1>
-          <div class="m-5 text-xl uppercase tracking-wide">
-            <p class="mb-5">Welcome Back!!</p>
-            <p class="mb-5">In person classes next fall!!</p>
-            <p class="mb-5">Open registration will commence in July</p>
-            <button class="bg-gradient-to-br from-green-500 to-green-700 p-2 rounded-xl text-white shadow-xl">Register Now!</button>
+        <div className="col-span-1 rounded-lg bg-gradient-to-br from-yellow-300 to-yellow-500 shadow-2xl">
+          <h1 className="text-center lg:text-3xl p-5 font-semibold">GOBIND SARVAR NEWS!</h1>
+          <div className="m-5 text-xl uppercase tracking-wide">
+            <p className="mb-5">Welcome Back!!</p>
+            <p className="mb-5">In person classes next fall!!</p>
+            <p className="mb-5">Open registration will commence in July</p>
+            <button className="bg-gradient-to-br from-green-500 to-green-700 p-2 rounded-xl text-white shadow-xl">Register Now!</button>
           </div>
         </div>
       </div>
