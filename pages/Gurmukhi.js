445ad1de5bd283809d24a4df72aa1fdbdef4bcc1
Akaljot Gujjar <akaljotgujjar@gmail.com>
2021-06-16 22:41:17 -0700

    added content to Gurmukhi and LKC page

--- pages/Gurmukhi.js
+++ pages/Gurmukhi.js
@@ -11,19 +11,64 @@ function Gurmukhi() {
 
       <Navbar />
 
-      <div className="m-10">
+      <main className="min-h-screen p-5">
+        <section className="container mx-auto mb-10">
+          <h1 className="md:text-5xl flex justify-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-blue-500">
+            Gurmukhi Level 1
+          </h1>
+        </section>
+
+        <section className="grid md:grid-cols-3 gap-4">
+          <div className="relative rounded-lg shadow-xl bg-white p-16 bg-gradient-to-br from-yellow-300 to-yellow-500">
+            <h3 className="text-gray-900 md:text-3xl font-bold mb-4 align-center">
+              Let Us Learn Gurmukhi (Book 1)
+            </h3>
+            <button className="hover:text-white md:text-lg border-solid border-2 border-red-800 p-2 rounded-xl">
+              {" "}
+              <a
+                href="https://archive.org/details/LetUsLearnGurmukhi-Book1/mode/2up"
+                alt="Let Us Learn Gurmukhi"
+                target="_blank"
+              >
+                Click Here!
+              </a>
+            </button>
+          </div>
+          <div className="relative rounded-lg shadow-xl bg-white p-16 bg-gradient-to-br from-yellow-300 to-yellow-500">
+            <h3 className="text-gray-900 md:text-3xl font-bold mb-4 align-center">
+              Let Us Learn Gurmukhi (Book 2)
+            </h3>
+            <button className="hover:text-white md:text-lg border-solid border-2 border-red-800 p-2 rounded-xl">
+              <a
+                href="https://archive.org/details/LetUsLearnGurmukhi-Book2/mode/2up"
+                alt="Let Us Learn Gurmukhi"
+                target="_blank"
+              >
+                Click Here
+              </a>
+            </button>
+          </div>
+        </section>
+      </main>
+
+      {/* <div className="m-10">
         <h1 className="lg:text-4xl text-center font-bold tracking-wider bg-clip-text text-transparent bg-gradient-to-r from-yellow-600 to-yellow-400 ">
           Gurmukhi Resources!
         </h1>
       </div>
 
-      <div className="bg-yellow-100 ">
-        <div className="h-120 w-80 m-auto bg-red-100">
+      <div className="">
+        <div className="h-120 w-80 m-auto">
           <h1 className="lg:text-3xl text-center font-bold tracking-wider">
             Gurmukhi Level 1
           </h1>
         </div>
-      </div>
+        <div className="grid md:grid-cols-3 gap-4 mt-5 p-5">
+          <div className="bg-yellow-100">1</div>
+          <div className="bg-blue-100">2</div>
+          <div className="bg-red-100">3</div>
+        </div>
+      </div> */}
     </div>
   );
 }
--- pages/LKC.js
+++ pages/LKC.js
@@ -11,7 +11,90 @@ function LKC() {
 
       <Navbar />
 
-      <h1>Little Khalsa Club Resources</h1>
+      <main className="min-h-screen p-5">
+        <section className="container mx-auto mb-10">
+          <h1 className="md:text-5xl flex justify-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-blue-500">
+            Little Khalsa Club
+          </h1>
+        </section>
+
+        <section className="grid md:grid-cols-3 gap-4">
+          <div className="relative rounded-lg shadow-xl bg-white p-16 bg-gradient-to-br from-yellow-300 to-yellow-500">
+            <h3 className="text-gray-900 md:text-3xl font-bold mb-4 align-center">
+              "We Are the Khalsa"
+            </h3>
+            <button className="hover:text-white md:text-lg border-solid border-2 border-red-800 p-2 rounded-xl">
+              {" "}
+              <a
+                href="https://www.youtube.com/watch?v=FohqXfdzXzc"
+                alt="We Are the Khalsa"
+                target="_blank"
+              >
+                Click Here!
+              </a>
+            </button>
+          </div>
+
+          <div className="relative rounded-lg shadow-xl bg-white p-16 bg-gradient-to-br from-yellow-300 to-yellow-500">
+            <h3 className="text-gray-900 md:text-3xl font-bold mb-4 align-center">
+              Guru Song
+            </h3>
+            <button className="hover:text-white md:text-lg border-solid border-2 border-red-800 p-2 rounded-xl">
+              <a
+                href="https://www.youtube.com/watch?v=8UXo_RnWerQ"
+                alt="Guru Song"
+                target="_blank"
+              >
+                Click Here
+              </a>
+            </button>
+          </div>
+
+          <div className="relative rounded-lg shadow-xl bg-white p-16 bg-gradient-to-br from-yellow-300 to-yellow-500">
+            <h3 className="text-gray-900 md:text-3xl font-bold mb-4 align-center">
+              Incy Wincy Khalsa
+            </h3>
+            <button className="hover:text-white md:text-lg border-solid border-2 border-red-800 p-2 rounded-xl">
+              <a
+                href="https://www.youtube.com/watch?v=lM1sC1jwnGE"
+                alt="Incy Wincy Khalsa Song"
+                target="_blank"
+              >
+                Click Here
+              </a>
+            </button>
+          </div>
+
+          <div className="relative rounded-lg shadow-xl bg-white p-16 bg-gradient-to-br from-yellow-300 to-yellow-500">
+            <h3 className="text-gray-900 md:text-3xl font-bold mb-4 align-center">
+              "The First Step"
+            </h3>
+            <button className="hover:text-white md:text-lg border-solid border-2 border-red-800 p-2 rounded-xl">
+              <a
+                href="https://www.youtube.com/watch?v=ejsjJg0BnIA"
+                alt="The First Step"
+                target="_blank"
+              >
+                Click Here
+              </a>
+            </button>
+          </div>
+          <div className="relative rounded-lg shadow-xl bg-white p-16 bg-gradient-to-br from-yellow-300 to-yellow-500">
+            <h3 className="text-gray-900 md:text-3xl font-bold mb-4 align-center">
+              Punjabi Alphabet
+            </h3>
+            <button className="hover:text-white md:text-lg border-solid border-2 border-red-800 p-2 rounded-xl">
+              <a
+                href="https://www.youtube.com/watch?v=B2GbaEW8NNM"
+                alt="Punjabi Alphabet"
+                target="_blank"
+              >
+                Click Here
+              </a>
+            </button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
